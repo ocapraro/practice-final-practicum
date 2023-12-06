defa7de3695d236863da38595c4298141bf72078
Oscar Capraro <oscar@mcapraro.com>
2023-12-06 10:57:34 -0500

    test: add Part 2 tests

--- practicum_test.py
+++ practicum_test.py
@@ -29,3 +29,42 @@ def test_create_book():
   assert publication_year == book.get_publication_year()
   assert compact == str(book)
   assert detailed == repr(book)
+
+# PART 2 TESTS
+def test_sort_books():
+  # setup
+  book1 = Book("Adventures of Huckleberry Finn", "Mark Twain", 1884)
+  book2 = Book("Iliad", "Homer", 1598)
+  book3 = Book("Fahrenheit 451", "Ray Bradbury", 1953)
+  book4 = Book("Dracula", "Bram Stoker", 1897)
+  book5 = Book("Animal Farm", "George Orwell", 1945)
+  books = [book1, book2, book3, book4, book5]
+
+  # invoke
+  copy = sorted(books)
+
+  # analyze
+  assert book4 == copy[0]
+  assert book5 == copy[1]
+  assert book2 == copy[2]
+  assert book1 == copy[3]
+  assert book3 == copy[4]
+
+def test_sort_books_duplicates():
+  # setup
+  book1 = Book("Animal Farm", "George Orwell", 1945)
+  book2 = Book("1984", "George Orwell", 1949)
+  book3 = Book("Fahrenheit 451", "Ray Bradbury", 1953)
+  book4 = Book("Adventures of Huckleberry Finn", "Mark Twain", 1884)
+  book5 = Book("Iliad", "Homer", 1598)
+  books = [book1, book2, book3, book4, book5]
+
+  # invoke
+  copy = sorted(books)
+
+  # analyze
+  assert book2 == copy[0]
+  assert book1 == copy[1]
+  assert book5 == copy[2]
+  assert book4 == copy[3]
+  assert book3 == copy[4]
