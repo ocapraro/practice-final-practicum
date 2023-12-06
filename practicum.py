2f3443192a20cf89839db3b0e64e677ae3d3814e
Oscar Capraro <oscar@mcapraro.com>
2023-12-06 10:17:46 -0500

    feat add basic practicum

---
+++ practicum.py
@@ -0,0 +1,22 @@
+"""
+Implement your solution to the practicum in this file.
+
+@author Your Name
+"""
+class Book:
+  pass
+
+class Library:
+  pass
+
+def main():
+    """
+    Use this function to manually test your code (if needed)
+    """
+
+def organize_library(library:Library):
+   pass
+   
+
+if __name__ == "__main__":
+    main()
